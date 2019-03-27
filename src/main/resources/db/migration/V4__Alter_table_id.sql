09660c570862d9214d86892ff44510a78e2987e4
Yao Liu <io.yao.liu@gmail.com>
2019-03-27 04:12:29 +0000

    Revert "Change recommendation Id to long"
    This reverts commit 180b520bb6de1bf2f7f80ec08627863a2bde8523

--- src/main/resources/db/migration/V4__Alter_table_id.sql
+++ src/main/resources/db/migration/V4__Alter_table_id.sql
@@ -1,7 +1,6 @@
 ALTER TABLE recommendation
     DROP FOREIGN KEY recommendation_ibfk_1,
-    MODIFY COLUMN portfolio_id BIGINT(20) UNSIGNED NOT NULL,
-    MODIFY COLUMN recommendation_id BIGINT(20) UNSIGNED NOT NULL;
+    MODIFY COLUMN portfolio_id BIGINT(20) UNSIGNED NOT NULL;
 
 ALTER TABLE portfolio MODIFY COLUMN portfolio_id BIGINT(20) UNSIGNED NOT NULL;
 
